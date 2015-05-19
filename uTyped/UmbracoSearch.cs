ca26f625a89ea40a35d10b2fb3f334260cf52c01
Nicolas Guelpa <nguelpa@deepfocus.net>
2015-05-19 17:38:44 -0400

    Improving search by adding the SearchProvider option

--- uTyped/UmbracoSearch.cs
+++ uTyped/UmbracoSearch.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Examine;
 using Examine.LuceneEngine.SearchCriteria;
+using Examine.Providers;
 using Examine.SearchCriteria;
 using System;
 using System.Collections.Generic;
@@ -37,10 +38,11 @@ namespace uTyped
         /// <param name="wildCard">Using wildCard or not</param>
         /// <param name="xPath">Path to the node. If not provided, the output class name is used</param>
         /// <param name="nodeTypeAlias">Alias for the node type in Umbraco</param>
+        /// <param name="searchProvider">Search provider</param>
         /// <returns></returns>
-        public IEnumerable<T> Search<T>(string term, float fuzzy = 0.5f, bool wildCard = true, string nodeTypeAlias = null, string xPath = null)
+        public IEnumerable<T> Search<T>(string term, float fuzzy = 0.5f, bool wildCard = true, string nodeTypeAlias = null, string xPath = null, BaseSearchProvider searchProvider = null)
         {
-            return Search<T>(term, fuzzy, wildCard, Mapper.Map<IEnumerable<T>>, nodeTypeAlias, xPath);
+            return Search<T>(term, fuzzy, wildCard, Mapper.Map<IEnumerable<T>>, nodeTypeAlias, xPath, searchProvider);
         }
 
         /// <summary>
@@ -52,15 +54,16 @@ namespace uTyped
         /// <param name="mapper">Function mapping the IPublishedContent to the expected out Type T</param>
         /// <param name="xPath">Path to the node. If not provided, the output class name is used</param>
         /// <param name="nodeTypeAlias">Alias for the node type in Umbraco</param>
+        /// <param name="searchProvider">Search provider</param>
         /// <returns></returns>
-        public IEnumerable<T> Search<T>(string term, float fuzzy, bool wildCard, Func<IEnumerable<IPublishedContent>, IEnumerable<T>> mapper, string nodeTypeAlias = null, string xPath = null)
+        public IEnumerable<T> Search<T>(string term, float fuzzy, bool wildCard, Func<IEnumerable<IPublishedContent>, IEnumerable<T>> mapper, string nodeTypeAlias = null, string xPath = null, BaseSearchProvider searchProvider = null)
         {
             //First getting a sample to retrieve all document type properties
             var content = _umbracoHelper.TypedContentSingleAtXPath(xPath ?? string.Format("//{0}", nodeTypeAlias ?? typeof(T).Name));
             if (null != content)
             {
                 var properties = content.ContentType.PropertyTypes.Select(p => p.PropertyTypeAlias).Concat(_nodeProperties);
-                return Search<T>(term, properties, mapper, fuzzy, wildCard, nodeTypeAlias);
+                return Search<T>(term, properties, mapper, fuzzy, wildCard, nodeTypeAlias, searchProvider);
             }
 
             return mapper(null);
@@ -73,10 +76,11 @@ namespace uTyped
         /// <param name="properties">List of properties to apply the fuzziness on and use for the search</param>
         /// <param name="fuzzy">The fuzzieness level</param>
         /// <param name="wildCard">Using wildCard or not</param>
+        /// <param name="searchProvider">Search provider</param>
         /// <returns></returns>
-        public IEnumerable<T> Search<T>(string term, IEnumerable<string> properties, float fuzzy = 0.5f, bool wildCard = true)
+        public IEnumerable<T> Search<T>(string term, IEnumerable<string> properties, float fuzzy = 0.5f, bool wildCard = true, string searchProvider = null)
         {
-            return Search<T>(term, properties, Mapper.Map<IEnumerable<T>>, fuzzy, wildCard);
+            return Search<T>(term, properties, Mapper.Map<IEnumerable<T>>, fuzzy, wildCard, searchProvider);
         }
 
         /// <summary>
@@ -88,8 +92,9 @@ namespace uTyped
         /// <param name="fuzzy">The fuzzieness level</param>
         /// <param name="wildCard">Using wildCard or not</param>
         /// <param name="nodeTypeAlias">Alias for the node type in Umbraco</param>
+        /// <param name="searchProvider">Search provider</param>
         /// <returns></returns>
-        public IEnumerable<T> Search<T>(string term, IEnumerable<string> properties, Func<IEnumerable<IPublishedContent>, IEnumerable<T>> mapper, float fuzzy = 0.5f, bool wildCard = true, string nodeTypeAlias = null)
+        public IEnumerable<T> Search<T>(string term, IEnumerable<string> properties, Func<IEnumerable<IPublishedContent>, IEnumerable<T>> mapper, float fuzzy = 0.5f, bool wildCard = true, string nodeTypeAlias = null, BaseSearchProvider searchProvider = null)
         {
             nodeTypeAlias = nodeTypeAlias ?? typeof(T).Name;
 
@@ -103,17 +108,18 @@ namespace uTyped
             var search = _examine.CreateSearchCriteria();
             var q = search.NodeTypeAlias(nodeTypeAlias).And().GroupedOr(properties, query.ToArray());
 
-            return Search<T>(q.Compile(), mapper);
+            return Search<T>(q.Compile(), mapper, searchProvider);
         }
 
         /// <summary>
         /// Search for the cirteria and returns an IEnumerable of T
         /// </summary>
         /// <param name="criteria">Criteria to search for</param>
+        /// <param name="searchProvider">Search provider</param>
         /// <returns></returns>
-        public IEnumerable<T> Search<T>(ISearchCriteria criteria)
+        public IEnumerable<T> Search<T>(ISearchCriteria criteria, BaseSearchProvider searchProvider = null)
         {
-            return Search<T>(criteria, Mapper.Map<IEnumerable<T>>);
+            return Search<T>(criteria, Mapper.Map<IEnumerable<T>>, searchProvider);
         }
 
         /// <summary>
@@ -121,20 +127,23 @@ namespace uTyped
         /// </summary>
         /// <param name="criteria">Criteria to search for</param>
         /// <param name="mapper">Function mapping the IPublishedContent to the expected out Type T</param>
+        /// <param name="searchProvider">Search provider</param>
         /// <returns></returns>
-        public IEnumerable<T> Search<T>(ISearchCriteria criteria, Func<IEnumerable<IPublishedContent>, IEnumerable<T>> mapper)
+        public IEnumerable<T> Search<T>(ISearchCriteria criteria, Func<IEnumerable<IPublishedContent>, IEnumerable<T>> mapper, BaseSearchProvider searchProvider = null)
         {
-            return mapper(_umbracoHelper.TypedSearch(criteria));
+            return mapper(_umbracoHelper.TypedSearch(criteria, searchProvider));
         }
 
         /// <summary>
         /// Search for the term and returns an IEnumerable of IPublishedContent
         /// </summary>
         /// <param name="term">Term to search</param>
+        /// <param name="useWildCards"></param>
+        /// <param name="searchProvider"></param>
         /// <returns></returns>
-        public IEnumerable<IPublishedContent> Search(string term)
+        public IEnumerable<IPublishedContent> Search(string term, bool useWildCards = true, string searchProvider = null)
         {
-            return _umbracoHelper.TypedSearch(term);
+            return _umbracoHelper.TypedSearch(term, useWildCards, searchProvider);
         }
     }
 }
